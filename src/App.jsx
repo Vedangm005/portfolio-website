e8d6861a7a623d29f54d0de741aa06b656ec01e0
VedangM005 <2430246@kiit.ac.in>
2026-03-01 21:56:32 +0530

    Initial React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="bg-black text-white h-screen flex items-center justify-center">
+
+      <h1 className="text-6xl font-bold">
+        Vedang Mishra
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
